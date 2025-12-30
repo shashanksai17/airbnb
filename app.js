be424928fc320d33056947407f14c2872cd59df4
shashanksai <shashanksai1703@gmail.com>
2025-12-30 16:22:10 +0530

    Redirect root route to /listings

--- app.js
+++ app.js
@@ -75,9 +75,9 @@ const sessionOptions = {
   },
 };
 
-// app.get("/",(req,res)=>{
-//     res.send("this site is safe");
-// })
+app.get("/",(req,res)=>{
+    res.redirect("/listings");
+})
 
 app.use(session(sessionOptions));
 app.use(passport.initialize());
